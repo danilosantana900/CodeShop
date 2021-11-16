2ecd7c31d5e862dc22f92cd003e2334506507914
Melissa Akatuka <melissa.akatsuka@gmail.com>
2021-11-15 23:57:54 -0300

    Adicionada classe Produto e ProdutoController

---
+++ CodeShop/Controllers/ProdutoController.cs
@@ -0,0 +1,30 @@
+﻿using CodeShop.Repository.DataModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Roles = "Funcionario")]
+        public IActionResult Get([FromServices] DataBase dataBase)
+        {
+            var result = dataBase.Usuario.ToList();
+            if (result.Any())
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return StatusCode(204, string.Empty);
+            }
+        }
+    }
+}
