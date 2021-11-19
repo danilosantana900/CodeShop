83bdcd40112142e5580859f8f8e43d1ccee8f826
Danilo dos Santos Alves Santana <danilosantana900@gmail.com>
2021-11-19 16:19:03 -0300

    Ajustes de identação do código e adicionado validação Role = CLiente para Carrinho.

--- CodeShop/Repository/DataModel/Item.cs
+++ CodeShop/Repository/DataModel/Item.cs
@@ -1,12 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace CodeShop.Repository.DataModel
+﻿namespace CodeShop.Repository.DataModel
 {
     public class Item
     {
         public int Id { get; set; }
         public Produto Produto { get; set; }
         public int Quantidade { get; set; }
-
     }
 }
